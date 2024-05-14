a00c4d2c15aaf44c74a437c8d81d085c61db2347
Fabian Gubler <fabian.gubler@hotmail.com>
2024-05-14 17:01:52 +0700

    feat(server): make simple http server

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
+type apiConfig struct {
+	serverHits int
+}
+
 func main() {
 	godotenv.Load(".env")
 
-	PORT := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-	fmt.Println(PORT)
+	apiCfg := apiConfig{
+		serverHits: 0,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
