d97e1687ab8ec8d5abd17320e512b5861ce4208e
Fabian Gubler <fabian.gubler@hotmail.com>
2024-05-15 17:58:01 +0700

    feat(db): add database functionality and post api

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/fabian-gubler/RSSFlow/internal/database"
 	"github.com/joho/godotenv"
 )
 
 type apiConfig struct {
 	serverHits int
+	DB         *database.Queries
 }
 
 func main() {
+
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONN")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v\n", err)
+	}
+
+	dbQueries := database.New(db)
 
 	apiCfg := apiConfig{
 		serverHits: 0,
+		DB:         dbQueries,
 	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /v1/readiness", apiCfg.handlerReadiness)
 	mux.HandleFunc("GET /v1/err", apiCfg.handlerErr)
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerCreateUser)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
